9bc77d6163ea01929f2261b1ce3997786e28fb62
programadormovel <programadormovel@gmail.com>
2025-04-29 10:17:58 -0300

    pokes

---
+++ src/PokeFlex.jsx
@@ -0,0 +1,11 @@
+import poke from './assets/pokeapi_256.png'
+import './PokeFlex.css'
+const PokeFlex = () => {
+    return (
+        <div 
+            className='Pokes'>
+            <img src={poke} />
+        </div>
+    )
+}
+export default PokeFlex
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import AppRouter from './AppRouter.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <AppRouter />
+  </BrowserRouter>,
 )
